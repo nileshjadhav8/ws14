7cd1b0082d27b6242a05e7e00df8ecb41a94593e
Nilesh <nil.y.jadhav@gmail.com>
2023-02-22 10:36:28 +0800

    initial comit

---
+++ src/main/java/com/practice14/practice14/Practice14Application.java
@@ -0,0 +1,13 @@
+package com.practice14.practice14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice14Application.class, args);
+	}
+
+}
